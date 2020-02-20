9c71ef0b760bd25f2a88d85f76c139c9e314993d
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-02-20 23:23:38 +0200

    create new func -> update in Paddle class and realize first logic gameLoop

--- src/paddle.js
+++ src/paddle.js
@@ -1,7 +1,7 @@
 class Paddle {
   constructor(gameWidth, gameHeight) {
     this.width = 150;
-    this.height = 30;
+    this.height = 20;
 
     this.position = {
       x: gameWidth / 2 - this.width / 2,
@@ -10,6 +10,12 @@ class Paddle {
   }
 
   draw(ctx) {
+    ctx.fillStyle = "#0ff";
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update(deltaTime) {
+    if (!deltaTime) return;
+    this.position.x += 5 / deltaTime;
   }
 }
