2183e2fb6e8b0e8c7d82c4e8d6ba91011b69198a
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-02-21 22:46:14 +0200

    add collision on paddle logic

--- src/ball.js
+++ src/ball.js
@@ -5,9 +5,11 @@ class Ball {
     this.gameWidth = game.gameWidth;
     this.gameHeight = game.gameHeight;
 
+    this.game = game;
+
     this.position = { x: 10, y: 10 };
-    this.speed = { x: 2, y: 2 };
-    this.size = 16;
+    this.speed = { x: 3, y: 3 };
+    this.size = 28;
   }
 
   draw(ctx) {
@@ -24,12 +26,30 @@ class Ball {
     this.position.x += this.speed.x;
     this.position.y += this.speed.y;
 
+    // wall on left or right
     if (this.position.x + this.size > this.gameWidth || this.position.x < 0) {
       this.speed.x = -this.speed.x;
     }
 
+    // wall on top or bottom
     if (this.position.y + this.size > this.gameHeight || this.position.y < 0) {
       this.speed.y = -this.speed.y;
+    }
+
+    // check collision with paddle
+    let bottomOfBall = this.position.y + this.size;
+    let topOfBall = this.game.paddle.position.y;
+    let leftSideOfPaddle = this.game.paddle.position.x;
+    let rightSideOfPaddle =
+      this.game.paddle.position.x + this.game.paddle.width;
+
+    if (
+      bottomOfBall >= topOfBall &&
+      this.position.x >= leftSideOfPaddle &&
+      this.position.x + this.size <= rightSideOfPaddle
+    ) {
+      this.speed.y = -this.speed.y;
+      this.position.y = this.game.paddle.position.y - this.size;
     }
   }
 }
