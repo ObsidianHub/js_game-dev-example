8415f747a6e33f2848dec06993559af1ffea43cd
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-02-21 13:53:45 +0200

    add update Ball logic (write conditions)

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ let ctx = canvas.getContext("2d");
 const GAME_WIDTH = 800;
 const GAME_HEIGHT = 600;
 
+let ball = new Ball(GAME_WIDTH, GAME_HEIGHT);
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
-let ball = new Ball();
 
 new InputHandler(paddle);
 
@@ -17,13 +17,15 @@ function gameLoop(timestamp) {
   let deltaTime = timestamp - lastTime;
   lastTime = timestamp;
 
-  ctx.clearRect(0, 0, 800, 600);
+  ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+
   paddle.update(deltaTime);
   paddle.draw(ctx);
 
+  ball.update(deltaTime);
   ball.draw(ctx);
 
   requestAnimationFrame(gameLoop);
 }
 
-gameLoop();
+requestAnimationFrame(gameLoop);
